e75ce33dbc8b63e1288e30c81dd1a1360ddaa929
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-03-25 14:13:44 +0100

    add filters

---
+++ apps/questions/static/Filters.jsx
@@ -0,0 +1,35 @@
+/* global django */
+let React = require('react')
+
+
+class Filter extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  selectCategory (e) {
+    e.preventDefault()
+    let category = e.target.getAttribute('data-value')
+    this.props.setCategories(category)
+  }
+
+  render() {
+    return (
+      <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {this.props.currentCategory}
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a className="dropdown-item" data-value={-1} onClick={this.selectCategory.bind(this)} href="#">{django.gettext('all')}</a>
+          { this.props.categories.map((category, index) => {
+            return <a className="dropdown-item" key={index} data-value={category} onClick={this.selectCategory.bind(this)} href="#">{category}</a>
+          })
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = Filter
