11e298d3b1dfc8fa5bfa4101760f7e3cfc9d0d1a
Robert Mader <r.mader@liqd.net>
2019-03-26 13:17:30 +0100

    enable whitenoise

--- a4-speakup/settings/base.py
+++ a4-speakup/settings/base.py
@@ -85,6 +85,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
