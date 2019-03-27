5578997c8c6c2f57fc6dcd94f9d0aa2bdfb0b637
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-03-27 13:57:05 +0100

    update rule for moderator

--- apps/questions/rules.py
+++ apps/questions/rules.py
@@ -2,20 +2,16 @@ import rules
 from rules.predicates import is_superuser
 
 from adhocracy4.modules.predicates import (is_context_initiator,
-                                           is_context_member,
-                                           is_context_moderator, is_owner)
-from adhocracy4.phases.predicates import (phase_allows_add,
-                                          phase_allows_change,
-                                          phase_allows_rate)
+                                           is_context_moderator)
+from adhocracy4.phases.predicates import phase_allows_add, phase_allows_rate
 
 from .models import Question
 
 rules.add_perm('a4-speakup_questions.rate_question', phase_allows_rate)
 
 
-rules.add_perm('a4-speakup_questions.modify_question',
-               is_superuser | is_context_moderator | is_context_initiator
-               | (is_context_member & is_owner & phase_allows_change))
+rules.add_perm('a4-speakup_questions.change_question',
+               is_superuser | is_context_moderator | is_context_initiator)
 
 
 rules.add_perm('a4-speakup_questions.propose_question',
