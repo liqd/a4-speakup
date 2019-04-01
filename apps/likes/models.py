a29bbad279caab838860209dbff71d1793d090a2
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-04-01 12:49:31 +0200

    add related name

--- apps/likes/models.py
+++ apps/likes/models.py
@@ -6,5 +6,5 @@ from apps.questions.models import Question
 
 class Like(models.Model):
     session = models.ForeignKey(Session)
-    question = models.ForeignKey(Question)
+    question = models.ForeignKey(Question, related_name='question_likes')
     created = models.DateTimeField(auto_now_add=True)
