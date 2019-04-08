cbb03c625f64e6496108a051b6c5fc2a5960a95d
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-04-08 19:52:18 +0200

    add serializer

---
+++ apps/likes/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Like
+        fields = ('question', 'id')
