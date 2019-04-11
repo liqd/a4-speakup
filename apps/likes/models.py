c08271b37b70d84aea9fb6879eeddf68b601baae
fuzzylogic2000 <katharina.albers@liqd.de>
2019-04-11 13:54:58 +0200

    fix likes model for session delete

--- apps/likes/models.py
+++ apps/likes/models.py
@@ -1,12 +1,13 @@
-from django.contrib.sessions.models import Session
 from django.db import models
 
 from apps.questions.models import Question
 
 
 class Like(models.Model):
-    session = models.ForeignKey(Session)
-    question = models.ForeignKey(Question, related_name='question_likes')
+    session = models.CharField(max_length=255)
+    question = models.ForeignKey(Question,
+                                 related_name='question_likes',
+                                 on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
