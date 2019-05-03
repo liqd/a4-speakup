28d1e76791a649a5dacd846c565c9127d08be3d3
fuzzylogic2000 <katharina.albers@liqd.de>
2019-05-03 15:28:54 +0200

    remove project update permission for moderators (and initiators)

--- apps/projects/rules.py
+++ apps/projects/rules.py
@@ -5,10 +5,6 @@ from adhocracy4.organisations.predicates import is_initiator
 from adhocracy4.projects.predicates import (is_live, is_member, is_moderator,
                                             is_public)
 
-rules.remove_perm('a4projects.change_project')
-rules.add_perm('a4projects.change_project',
-               is_superuser | is_initiator | is_moderator)
-
 rules.remove_perm('a4projects.view_project')
 rules.add_perm('a4projects.view_project',
                is_superuser | is_initiator | is_moderator
