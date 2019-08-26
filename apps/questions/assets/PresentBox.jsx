71873371201414dc72a498589eb15b8c6ad37bf2
fuzzylogic2000 <katharina.albers@liqd.de>
2019-08-26 16:30:53 +0200

    apps/questions: put the infograhic in the content and give it a bit more space

--- apps/questions/assets/PresentBox.jsx
+++ apps/questions/assets/PresentBox.jsx
@@ -65,8 +65,8 @@ class PresentBox extends React.Component {
       )
     } else {
       return (
-        <div className="row justify-content-center">
-          <div className="col-8 text-center">
+        <div className="row justify-content-center mt-5">
+          <div className="col-8 text-center py-5">
             <h1>{this.props.title}</h1>
             <p>{django.gettext('join in')} <a href={this.props.url}>{this.props.url}</a></p>
           </div>
